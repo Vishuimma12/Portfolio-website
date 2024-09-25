6d505493caf6cb5d665b39f278033066a2e1c5db
Vishal Singh <101267652+Vishuimma12@users.noreply.github.com>
2024-09-25 13:07:50 +0000

    Including three js

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './Components/Navbar'
+import Landing from './Components/Landing'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Landing/>
     </>
   )
 }
