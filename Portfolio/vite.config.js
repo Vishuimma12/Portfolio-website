c95364f3a0e679827227b73176ab4a91c75e53d7
Vishal Singh <101267652+Vishuimma12@users.noreply.github.com>
2024-10-08 18:24:15 +0000

    updated

--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,  // Increase limit to 1000 kB (or any other desired limit)
+  }
 })
