bb3c15edaffc2891c8fc099193a6a3d836d42c0f
Vishal Singh <101267652+Vishuimma12@users.noreply.github.com>
2024-10-08 19:07:57 +0000

    test 2

--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/', // Adjust this to your base URL if deploying to a subpath
+  base: 'https://6705814853d3530008065616--sage-syrniki-d3509d.netlify.app/', // Adjust this to your base URL if deploying to a subpath
   plugins: [react()],
   build: {
     outDir: 'dist', // Ensure this matches the directory structure
